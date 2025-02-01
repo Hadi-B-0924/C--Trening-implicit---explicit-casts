7b4789cf23acb7679fbde6c381e56efb3dbad6d5
Hadi-B-0924 <baih092024@gmail.com>
2025-02-01 21:08:53 +0100

    add mySecondWholeNumber and divisionResult

--- obj/Debug/net8.0/C# Trening implicit & explicit casts.AssemblyInfo.cs
+++ obj/Debug/net8.0/C# Trening implicit & explicit casts.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("C# Trening implicit & explicit casts")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d69620c2ff303c4f0d7942e5c21742ef6c79c926")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+972658d768dd12b30709ead62d8a9c6b4618fbe6")]
 [assembly: System.Reflection.AssemblyProductAttribute("C# Trening implicit & explicit casts")]
 [assembly: System.Reflection.AssemblyTitleAttribute("C# Trening implicit & explicit casts")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
