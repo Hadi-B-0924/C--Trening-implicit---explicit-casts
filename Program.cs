e03180bd8c1cd786c4b46f94fe4e6256a239d9ce
Hadi-B-0924 <baih092024@gmail.com>
2025-02-01 17:40:00 +0100

    add charResult

--- Program.cs
+++ Program.cs
@@ -18,7 +18,10 @@ class Program
         Console.WriteLine(numberResult.GetType()); 
         //consol logger en double, fordi double overvinner en int ettersom double består av dobbelt så mange datapakker som en int
         //med andre ord kan en double holde verdien til en int, men en int kan ikke holde på verdien til en double
-        
+        var charResult = myChar + myChar;
+        Console.WriteLine(charResult);
+        //vi får 194 i loggen da en char er en måte å holde informasjon om et tall på. 
+        //en char får tildelt 16 datapakker og bruker alle 16 for å holde informasjonen om dette tallet. 
 
 
     }
